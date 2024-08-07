72619a8bcd7f40cad646491a783ddde6a2d4328d
Sanjay Pachauri <pachuarisanjy08@gmail.com>
2024-08-07 15:16:35 +0530

    slider

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { IconButton, Card } from 'react-native-paper';
-import tw from "twrnc"
 import Home from './Components/Firstpage/Home'
 import Info from './Components/Profile/Info'
-import Sjob from './Components/Secondpage/Sjob'
-import Aboutme from './Components/Profile/Aboutme'
-import Msg from './Components/Notification/Msg'
-import Sjobcard from './Components/Secondpage/Sjobcard';
-import Spage from './Components/Secondpage/Spage';
-import SearchJobPage from './Components/SearchJobPage/SearchJobPage';
-import Filter from './Components/Filter/Filter';
+import SearchJobPage from './Components/SearchJobPage/SearchJobPage'
 
 const App = () => {
   return (
     <View>
-  <SearchJobPage/>
+      <SearchJobPage/>
     </View>
   )
 }
